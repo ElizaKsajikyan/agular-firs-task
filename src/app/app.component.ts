4ddb437414e8340cdad35ce39b68eccf3bf28089
Eliza <eliza.ksajikyan.h@gmail.com>
2018-07-25 11:44:06 +0400

    firs and second tasks angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,30 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // task one
   userInfo = {
     name: 'Eliza',
     surname: 'Ksajikyan',
     age: 29,
     email: 'eliza.ksajikyan.h@gmail.com'
   };
+  showUser = true;
 
   removeUserList() {
-    this.userInfo = {};
+    this.showUser = false;
+  }
+
+  // task two
+  users = [
+    {name: 'Karine', login: 'KarineK', password: '234567'},
+    {name: 'Ani', login: 'AniAni', password: '234567'},
+    {name: 'Hermine', login: 'Hermin', password: '67'},
+    {name: 'Jon', login: 'JonJon', password: '67jsakhdk'},
+  ];
+
+  checkUserName(user) {
+    if (user.name.indexOf('A') === -1) {
+      return user;
+    }
   }
 }
